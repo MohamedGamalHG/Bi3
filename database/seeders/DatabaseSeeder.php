f6fb0afad507bba3994373e20c0849d226dac9d5
MohamedGamalHG <49466286+MohamedGamalHG@users.noreply.github.com>
2022-08-12 22:44:29 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CategorySeeder::class);*/
         $this->call(CategorySeeder::class);
         $this->call(UserSeeder::class);
-        $this->call(SubCategorySeeder::class);
+        //$this->call(SubCategorySeeder::class);
     }
 }
