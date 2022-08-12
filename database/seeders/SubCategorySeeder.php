6cef098bb650005e57ed13078bb3d7ea9b20b9f4
MohamedGamalHG <49466286+MohamedGamalHG@users.noreply.github.com>
2022-08-12 22:34:02 +0200

    Add files via upload

---
+++ database/seeders/SubCategorySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\ٍSubCategory;
+
+class SubCategorySeeder extends Seeder
+{
+    Db::table('sub_categories')->delete();
+    $cat = [
+        ['ar' => 'موبيلات','en'=>'Mobiles'],
+        ['ar' => 'عربيات','en'=>'Cars'],
+    ];
+    /*foreach ($cat as $cats)
+    Db::table('categories')->insert(['category_name'=>$cats]);*/
+    $i = 1;
+  foreach ($cat as $cats)
+      Category::create(['subcategory_name'=>$cats,'category_id'=>$i++]);
+
+}
